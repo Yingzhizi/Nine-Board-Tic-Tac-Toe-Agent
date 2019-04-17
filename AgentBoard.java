93fb930f40ccdbb00ebf0e7100f87f5bc2bd3ad0
BoyLondon <fucshu@163.com>
2019-04-17 23:00:48 +1000

    add game over

--- AgentBoard.java
+++ AgentBoard.java
@@ -3,9 +3,9 @@
  * Dependency: GameState.java
  * 
  * 
- *TODO 1.Simulate the board
- *TODO 2.Simplify board operation
- *TODO 3.Display the board
+ *1.Simulate the board
+ *2.Simplify board operation
+ *3.Display the board
  * 
  * AgentBoard
  */
@@ -37,6 +37,54 @@ public class AgentBoard {
         board[num-1][(num_cell-1)/3][(num_cell-1)%3] = val;
     }
 
+    public boolean check_player_win(char x){
+        for(int i=0; i<9; i++){
+            for(int j=0; j<3; j++){
+                // check whether x is win on ROW
+                if(board[i][j][0] == board[i][j][1] && board[i][j][0] == board[i][j][2] && board[i][j][0] == x){
+                    return true;
+                }// check whether x is win on COLUMN
+                else if(board[i][0][j] == board[i][1][j] && board[i][0][j] == board[i][2][j] && board[i][0][j] == x){
+                    return true;
+                }// check whether x is win on SLOPE
+                else if(board[i][0][0] == board[i][1][1] && board[i][0][0] == board[i][2][2] && board[i][0][0] == x){
+                    return true;
+                }
+                else if(board[i][0][2] == board[i][1][1] && board[i][0][2] == board[i][2][0] && board[i][0][2] == x){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean game_over(){
+        if (check_player_win('x')){
+            unitState = GameState.GameOver;
+            return true;
+        }else if(check_player_win('o')){
+            unitState = GameState.GameOver;
+            return true;
+        }
+
+        return false;
+    }
+
+    // Display the board like this:
+    /*
+        . . . * . . . * . . . 
+        . . . * . . . * . . . 
+        . . . * . . . * . . o 
+        * * * * * * * * * * * 
+        . . . * . . o * . . . 
+        . . . * . . . * . . . 
+        . . . * . . . * . . . 
+        * * * * * * * * * * * 
+        o . . * . . . * . . . 
+        . . . * . . . * . . . 
+        . . . * . . . * . . . 
+    */
+
     public void display_board(){
 
         for(int row=0; row<9; row++){ 
@@ -66,7 +114,11 @@ public class AgentBoard {
         agent_board.set_val(5, 3, 'o');
         agent_board.set_val(3, 9, 'o');
         agent_board.set_val(7, 1, 'o');
+        agent_board.set_val(7, 5, 'o');
+        
         agent_board.display_board();
+        System.out.println(agent_board.check_player_win('o'));
+        System.out.println(agent_board.game_over());
     }
 
 

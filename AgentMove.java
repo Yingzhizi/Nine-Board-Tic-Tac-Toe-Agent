b77e11cb27362d01881f3f0be8ddebd385eec6e1
BoyLondon <fucshu@163.com>
2019-04-21 17:58:59 +1000

    working on ab prune

--- AgentMove.java
+++ AgentMove.java
@@ -8,6 +8,7 @@ public class AgentMove {
     /* default agent, modify at the beginning */
     private char agent = 'o';
     private char opponent = 'x';
+    private AgentBoard bd;
 
     public AgentMove(char agent_char){
         if (agent_char == 'x'){
@@ -15,6 +16,7 @@ public class AgentMove {
             agent = 'x';
             opponent = 'o';
         }
+        bd = new AgentBoard();
     }
 
     public char switch_player(){
@@ -27,24 +29,36 @@ public class AgentMove {
         }
     }
 
-    public int alpha_beta(char player, int alpha, int beta){
-        if(check_player_win(agent)){
+    public int alpha_beta(AgentBoard bd, int cell, char player, int alpha, int beta){
+        if(bd.cell_check_player_win(cell, agent)){
             return 100;
-        }else if(check_player_win(opponent)){
+        }else if(bd.cell_check_player_win(cell, opponent)){
             return -100;
-        }else if (is_full()){
+        }else if (bd.cell_is_full(cell)){
             return 0;
         }
 
         if (player == opponent){
+            System.out.println(bd.can_move(cell));
+            locations = bd.can_move(cell);
+            for(int i=0; i<locations.size(); i++){
+                System.out.println(locations[i]);
+            }
+        }else{
+            System.out.println(bd.can_move(cell));
 
         }
-
+        return 0;
 
     }
 
     public static void main(String[] args) {
-        
+        System.out.println("agent move!");
+        AgentMove move = new AgentMove('o');
+        int alpha = -999999;
+        int beta = 999999;
+        int cell = 5;
+        move.alpha_beta(move.bd, cell, move.player, alpha, beta);
     }
 
 }

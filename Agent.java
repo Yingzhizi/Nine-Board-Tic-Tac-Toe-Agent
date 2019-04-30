a48bde50b0225a5b096ccabecd36cecd84eabf05
BoyLondon <fucshu@163.com>
2019-05-01 00:13:02 +1000

    add more comments

--- Agent.java
+++ Agent.java
@@ -7,10 +7,33 @@
 *  Firstly, we created a three-dimensional array to store the 9x9 
 *  in the class AgentBoard, and we divided the board into 9 sub-boards.
 *  The sub-board, also named cell by us, has index 1-9 in board[0-8][i][j]
-*  and i is the index for row, j for column. We set every position 
-*
+*  and i is the index for row, j for column. We gave every position a 
+*  initial value '.', and we manipulate the board by directly set the array
+*  value. Moreover, we created a singleton agent which is the instance
+*  of class AgentMove, it has a 9x9 board as member variable, and every
+*  move is recorded in this board.
+*  
+*  Basically, the agent's reaction towards the opponent move is to get the
+*  move through searching the game tree by alpha-beta pruning algorithm, we
+*  use recurrsion to implement this algorithm and tried to implement killer
+*  heuristic. In order to speed up, we search and store killer moves using
+*  a two-dimentional array for every level since the game situation is similar
+*  when it has the same level. Moreover, we set a few rules to get move before
+*  alpha-beta searching. For example, if the agent have a winning more, then
+*  move it and if the opponent has a winning move and this move wouldn't lead
+*  to an opponent winning board, then this move may be blocked by the agent.
+*  Furthermore, during alpha-beta pruning, if the search level equals to 0,
+*  we use heuristic function to evaluate the board. We invented heuristic 
+*  function and adjust heuristic parameters by analysing the move outcome and
+*  observing the move the lookt made through the final board when Agent playing
+*  against the lookt. The heuristic function is a combination of different 
+*  heuristic functions. e.g. when in the cell opponent has two connected, then
+*  we punish the move to that cell and decrease heuristic value and if the remain 
+*  positions of the cell which the move went to could lead the opponent to win,
+*  then we award the move and increase the heuristic value. Moreover, we observed
+*  that the winning positions diversity which means winning positions are different 
+*  between cells may be a good heuristic through using the agent play with looket.
 */
-
 
 
 import java.io.BufferedReader;
@@ -23,19 +46,12 @@ import java.util.ArrayList;
 public class Agent {
 
     // save the cell we need to locate
-    static int prevMove = 0;
-    static AgentBoard boards;
+    static AgentMove agentMove;
     static char player;
     static char opponent;
-    static AgentMove agentMove = AgentMove.getAgent();
 
     public Agent() {}
 
-    /* 0 = Empty
-     * 1 = I played here
-     * 2 = They played here
-     * Zero index not used
-     */
     public static void main(String args[]) throws IOException {
 
     AgentSubstitute newPlayer = new AgentSubstitute();
@@ -71,29 +87,20 @@ public class Agent {
 	}
     }
 
-    public AgentBoard getBoards() {
-        return boards;
-    }
-
     public static int parse(String line) {
 
-        // In agent.java If you receive second_move(6,5) command
-        // that means you play 'o' and the first move is 'x' in the 5th of the cell 6
-        // And If you receive third_move(6,5,7) command
-        // that means you play 'x' and your first move is 'x' in 5th of the cell 6 randomly set by teacher
-
         if(line.contains("init")) {
-            // initialize the agent board
-            
+            // initialize the Agent
+            agentMove = AgentMove.getAgent();
 
         }else if(line.contains("start")) {
             int argsStart = line.indexOf("(");
             int argsEnd = line.indexOf(")");
-            // now we get the agent type
+
             String list = line.substring(argsStart+1, argsEnd);
             // System.out.println(line);
             char type = list.charAt(0);
-            
+            //  set the agent type and opponent type by setAgent
             agentMove.setAgent(type);
             System.out.println("Player is " + type);
             
